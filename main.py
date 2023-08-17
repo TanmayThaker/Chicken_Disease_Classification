69c05002f0e8c6ffa98e3cdeb9317a29096099d7
tanmay-NairaCare <132326225+tanmay-NairaCare@users.noreply.github.com>
2023-08-17 14:51:40 +0530

    Stage 02 added Preapre Base Model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ logger.info("Welcome to custom logger")
 
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -14,3 +15,12 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+STAGE_NAME  = 'Prepare base model'
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
